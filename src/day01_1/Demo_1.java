47d58c038a718eecc20146bc6394a115a3cbe7cd
hanzeqiang <13474124981@163.com>
2018-05-16 00:42:41 +0800

    day01_1

---
+++ src/day01_1/Demo_1.java
@@ -0,0 +1,21 @@
+package day01_1;
+//演示java中的所有类型常量
+public class Demo_1 {
+	public static void main(String[] args) {
+		//输出整数 十进制
+		System.out.println(23);
+		//输出整数，二进制。数字开头0B
+		System.out.println(0B10);
+		//输出整数，八进制 ，数字开头0
+		System.out.println(012);
+		//输出整数，十六进制 ，数字开头0x
+		System.out.println(0xA1);
+		//输出浮点
+		System.out.println(5.0);
+		//输出布尔类型
+		System.out.println(true);
+		System.out.println(false);
+		//输出字符,单引号包裹，只能写一个字符
+		System.out.println('A');
+	}
+}
---
+++ src/day01_1/day01_Learn.java
@@ -0,0 +1,17 @@
+package day01_1;
+//Day01¿ÎºóÁ·Ï°
+public class day01_Learn{
+	
+		public static void main(String[] args) 
+		{
+			System.out.println(15);//15
+			System.out.println(0B1111);//15
+			System.out.println(0xF);//15
+			System.out.println(017);//15
+			
+			System.out.println(true);
+			System.out.println(false);
+
+		}
+	}
+
